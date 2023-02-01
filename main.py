92e487c1ab3e270dc239e97d29b7b690bae9bc16
vaseem0 <75573349+vaseem0@users.noreply.github.com>
2023-02-01 20:08:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+import requests
+from bs4 import BeautifulSoup
+
+app = Flask(__name__)
+
+@app.route('/get')
+def notifications():
+    url = 'http://akgsma.com'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        updates = soup.find_all('div', {'class': 'col-sm-6'})
+        return jsonify([update.text for update in updates])
+    else:
+        return jsonify({'error': f'Request to {url} failed with status code {response.status_code}'})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
