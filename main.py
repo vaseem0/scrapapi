a2c2d3b61e781772cec47c75bd9e872237aec304
vaseem0 <75573349+vaseem0@users.noreply.github.com>
2023-02-01 22:18:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,4 +17,4 @@ def notifications():
         return jsonify({'error': f'Request to {url} failed with status code {response.status_code}'})
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(debug=True)
